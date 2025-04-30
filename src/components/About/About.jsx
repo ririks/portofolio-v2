28fd3239a0a46efc135abb7a2fbe312e3840d4d5
ririks <ririkomalasari870@gmail.com>
2025-04-30 14:21:49 +0700

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src="/public/about/aboutImage.png"
+          src="/about/aboutImage.png"
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
